aec732d5d31dda4609d740c953e6527434c9d026
Ekillz <8539715+Ekillz@users.noreply.github.com>
2016-07-18 17:41:42 +0200

    added weird 3d scene

---
+++ Assets/script/bsll_physic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bsll_physic : MonoBehaviour {
+
+	public Rigidbody rb;
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		//rb.AddForce(transform.forward * 700f, ForceMode.Impulse);
+	}
+	// Update is called once per frame
+	void Update () {
+//		rb.velocity = new Vector3 (0, 0, 500f);
+		float y = Terrain.activeTerrain.SampleHeight(transform.position);
+		transform.position = new Vector3(transform.localPosition.x, y + 6 , transform.localPosition.z);
+		transform.localPosition += new Vector3(0, 0, 5f);
+	}
+}
